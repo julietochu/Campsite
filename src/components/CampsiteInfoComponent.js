bc36fd22e133b9f8925855cb0e9f84f4bfbb5fa7
juliet ochu <52784536+julietochu@users.noreply.github.com>
2022-03-09 17:00:50 -0700

    breadcrumbs, Routes

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-} from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
   function RenderCampsite({campsite}) {
     return (
@@ -13,8 +8,7 @@ import {
         <Card>
           <CardImg top src={campsite.image} alt={campsite.name} />
           <CardBody>
-            <CardTitle>{campsite.name}</CardTitle>
-            <CardText>{campsite.description}</CardText>
+              <CardText>{campsite.description}</CardText>
           </CardBody>
         </Card>
       </div>
@@ -46,12 +40,22 @@ import {
   function CampsiteInfo(props)  {
     if (props.campsite) {
         return (
-            <div className="container">
-                <div className="row">
-                    <RenderCampsite campsite={props.campsite}/>
-                    <RenderComments comments={props.campsite.comments}/>
-                </div>
-            </div>
+          <div className="container">
+          <div className="row">
+              <div className="col">
+                  <Breadcrumb>
+                      <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                      <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                  </Breadcrumb>
+                  <h2>{props.campsite.name}</h2>
+                  <hr />
+              </div>
+          </div>
+          <div className="row">
+              <RenderCampsite campsite={props.campsite} />
+              <RenderComments comments={props.comments} />
+          </div>
+      </div>
         );
     }
     return <div />;
